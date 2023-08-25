0124593236c0dc153c3026504558fc1fd9092a42
Vlatko Nikolovski <vlatkon2007@gmail.com>
2023-08-25 14:26:26 +0200

    update license info

--- src/MinimalMediator.Abstractions/DependencyInjection/TrimmerNetstandardPolyfill.cs
+++ src/MinimalMediator.Abstractions/DependencyInjection/TrimmerNetstandardPolyfill.cs
@@ -1,4 +1,7 @@
-﻿// ReSharper disable CheckNamespace
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable CheckNamespace
 namespace System.Diagnostics.CodeAnalysis.Polyfill
 {
     [Flags]
